2c786daa645ad2300ef4823c63b78fc60a2f4e84
benjamin-abebe <benjamin.abebe@mths.ca>
2024-09-19 11:22:35 -0400

    Added function and changed the comment

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Benjamin Abebe 
+ * Created on: Sep 2024 
+ * This program Displays the current temperature. 
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+
+let currentTemperature = input.temperature()
+
+input.onButtonPressed(Button.A, function () {
+    basic.showString("The current temperature is: " + currentTemperature)
+})
+
+
