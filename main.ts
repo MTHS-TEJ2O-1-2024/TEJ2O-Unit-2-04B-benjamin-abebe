2832745d9043640faa0f3a020dce397644f926af
benjamin-abebe <benjamin.abebe@mths.ca>
2024-09-20 10:58:44 -0400

    Changed the variables and the value

--- main.ts
+++ main.ts
@@ -4,14 +4,12 @@
  * Created on: Sep 2024 
  * This program Displays the current temperature. 
 */
+let currentTemperature = input.temperature()
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
-
-let currentTemperature = input.temperature()
 
 input.onButtonPressed(Button.A, function () {
     basic.showString("The temperature is: " + currentTemperature)
 })
 basic.clearScreen()
-
